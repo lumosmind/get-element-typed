3d345c6b5676faf23079721bae1289e11727fd4a
Mustafa Kemal Tuna <mustafakemaltuna@hacettepe.edu.tr>
2021-02-14 15:31:11 +0300

    implement getElementTyped

---
+++ index.ts
@@ -0,0 +1,13 @@
+export default function getElementTyped<T extends HTMLElement>(selector: string, tagName: string): T | never {
+  const element = document.getElementById("user-input");
+
+  if (!element) throw new Error("element can not found");
+
+  const elementTagName = element.tagName
+  if (elementTagName == tagName) {
+    return element as T;
+  } else {
+    throw new Error(`tagName does not match expected:${tagName} found:${elementTagName}`);
+  }
+}
+
