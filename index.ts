f21963b5acbb5ecac135ef76325d00f710b1c4fa
Mustafa Kemal Tuna <mustafakemaltuna@hacettepe.edu.tr>
2021-02-15 01:25:51 +0300

    new way for calling function without generic type argument

--- index.ts
+++ index.ts
@@ -11,6 +11,19 @@ export default function getElementTyped<T extends HTMLElement>(selector: string,
   }
 }
 
+// Arda tanrıkulu nun çözümü, fonksiyonu çağırırken tip belirtmeye gerek kalmıyor
+//fonksiyon tipi tagName argümanından infer ediyor.
+export function getElementTyped2<TElementName extends keyof HTMLElementTagNameMap, TElement extends HTMLElementTagNameMap[TElementName]>(selector: string, tagName: TElementName): TElement | never {
+  const element = document.querySelector(selector);
 
-//todo : bunu anla ve uygulamaya çalış
-// getElementTyped<TElement extends keyof HTMLElementTagNameMap, TElement extends HTMLElementTagNameMap[TElementName]>(selector:string, tagName: TElementName): TElement | never
+  if (!element) throw new Error("element can not found");
+
+  const elementTagName = element.tagName.toLowerCase();
+  if (elementTagName == tagName) {
+    return element as TElement;
+  } else {
+    throw new Error(`tagName does not match expected:${tagName} found:${elementTagName}`);
+  }
+}
+
+//getElementTyped2("#user-input", "input");
